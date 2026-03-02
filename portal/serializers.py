ce7d80f220d7d8ed109dc20369c8b3b743707258
Mirza <mohammedmirsa010@gmail.com>
2026-03-03 02:13:55 +0530

    fix: Map underlying username, first_name, and last_name fields into ProfileSerializer to properly display on Directory screen cards

--- portal/serializers.py
+++ portal/serializers.py
@@ -2,10 +2,15 @@ from rest_framework import serializers
 from .models import User, Profile, Post, Job, JobApplication, Event, RSVP, Donation, FundAllocation, Community, Message
 class ProfileSerializer(serializers.ModelSerializer):
     profile_picture_url = serializers.SerializerMethodField()
+    username = serializers.CharField(source='user.username', read_only=True)
+    first_name = serializers.CharField(source='user.first_name', read_only=True)
+    last_name = serializers.CharField(source='user.last_name', read_only=True)
+    email = serializers.CharField(source='user.email', read_only=True)
+    role = serializers.CharField(source='user.role', read_only=True)
 
     class Meta:
         model = Profile
-        fields = ['bio', 'reg_no', 'department', 'graduation_year', 'degree', 'current_company', 'id_card', 'profile_picture', 'profile_picture_url']
+        fields = ['username', 'first_name', 'last_name', 'email', 'role', 'bio', 'reg_no', 'department', 'graduation_year', 'degree', 'current_company', 'id_card', 'profile_picture', 'profile_picture_url']
 
     def get_profile_picture_url(self, obj):
         if obj.profile_picture:
