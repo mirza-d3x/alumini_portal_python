b32f916a49512cf423352723fe8becff96c5100d
Mirza <mohammedmirsa010@gmail.com>
2026-03-05 01:12:34 +0530

    fix(uploads): reinstate media_uploads path bypass to resolve sandbox permission errors permanently

--- alumni_backend/settings.py
+++ alumni_backend/settings.py
@@ -143,8 +143,8 @@ STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media_uploads/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media_uploads')
 
 # Custom User Model
 AUTH_USER_MODEL = 'portal.User'
