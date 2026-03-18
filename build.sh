5dc45d64543889ff2f8556b8ce807d6213581541
Mirza <mohammedmirsa010@gmail.com>
2026-03-18 12:24:20 +0530

    Add reset_admin.py to build step for Free Deploy admin creation

--- build.sh
+++ build.sh
@@ -7,3 +7,6 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py makemigrations
 python manage.py migrate
+
+echo "=== Setting up Admin user ==="
+python reset_admin.py
